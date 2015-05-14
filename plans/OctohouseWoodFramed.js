22b336abb7053169b7f39daf86e032c18c3f468a
Izzy <yzziizzy@gmail.com>
2015-05-13 22:40:54 -0700

    octohouse file

---
+++ plans/OctohouseWoodFramed.js
@@ -0,0 +1,42 @@
+
+
+
+var studWall = require('./parts/walls').studWall;
+var prismRoof = require('./parts/roofs').prismRoof;
+var slabFoundation = require('./parts/foundations').slab;
+
+
+
+
+module.exports = {
+	components: Octohouse(sidelen, roofHeight) {
+		
+		var len = sidelen + (sidelen * 2 / Math.sqrt(2));
+		console.log(len);
+		return [
+			studWall(sidelen),
+			studWall(sidelen),
+			studWall(sidelen),
+			studWall(sidelen),
+			studWall(sidelen),
+			studWall(sidelen),
+			studWall(sidelen),
+			studWall(sidelen),
+			prismRoof(len, len, roofHeight),
+			slabFoundation(len, len, 8),
+		];
+	},
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
