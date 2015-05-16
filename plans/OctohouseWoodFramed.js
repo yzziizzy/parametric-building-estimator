e06e0325d2897d207cf2b80daa96608e7613906b
Izzy <yzziizzy@gmail.com>
2015-05-15 22:14:10 -0700

    changed format of plan file

--- plans/OctohouseWoodFramed.js
+++ plans/OctohouseWoodFramed.js
@@ -8,9 +8,9 @@ var slabFoundation = require('./parts/foundations').slab;
 
 
 
-module.exports = {
-	components: function(sidelen, roofHeight) {
-		
+module.exports = function(options) { return {
+	components: function() {
+		var sidelen = options.sideLength;
 		var len = sidelen + (sidelen * 2 / Math.sqrt(2));
 		console.log(len);
 		return [
@@ -22,11 +22,16 @@ module.exports = {
 			studWall(sidelen),
 			studWall(sidelen),
 			studWall(sidelen),
-			prismRoof(len, len, roofHeight),
-			slabFoundation(len, len, 8),
+			prismRoof(len, len, options.roofHeight),
+			slabFoundation(len, len, options.foundationDepth || 8),
 		];
 	},
 	
+	options: { 
+		{ name: 'sideLength', unit: 'feet', desc: 'Length of one of the 8 sides' },
+		{ name: 'roofHeight', unit: 'feet', desc: 'Roof height from top of walls' },
+		{ name: 'foundationDepth', unit: 'inches', def: 8, desc: 'Thickness of foundation slab' },
+	],
 	
 }
 
