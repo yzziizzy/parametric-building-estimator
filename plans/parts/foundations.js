7956b673a311fe8ed3653748aefab23867c16a67
Izzy <yzziizzy@gmail.com>
2015-05-18 03:31:58 -0700

    new parts

--- plans/parts/foundations.js
+++ plans/parts/foundations.js
@@ -17,6 +17,35 @@ function slab(length, width, thickness) {
 
 
 
+function platform(length, width, thickness) {
+	
+	var jspacing = 8 / 12;
+	
+	var max_span = 12;
+	
+	var  l = max(length, width);
+	var  w = min(length, width);
+	
+	var across = ceil(l / jspacing);
+	
+	return Component({
+		floor_joist: [
+		
+			// TODO: need a way to specify only continuous boards. 
+			CompMember(w, ceil(l / jspacing)), // joists across the width
+			CompMember(l, 2), // sides
+			CompMember(l, floor(w / max_span)), // beams
+		],
+		floor_decking: CompMember(d(w, l), 1),
+		flooring: CompMember(1, length * width),
+		joist_hangars: CompMember(1, 2 * ceil(l / jspacing) * floor(w / max_span)) // two for every joist, per span
+// 		concrete: // under pads
+		// supports of whatever kind
+	});
+};
+
+
+
 
 
 
