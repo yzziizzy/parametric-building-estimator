f0566fa2800762f57725311ca7adc0b71b87a3f0
Izzy <yzziizzy@gmail.com>
2015-08-17 20:55:30 -0700

    options file for the L1 plan

---
+++ L1options.js
@@ -0,0 +1,50 @@
+
+
+module.exports = {
+	plan: 'L1',
+	from: {
+		length: 30,
+		width: 30,
+		endLength: 15,
+		endWidth: 15,
+		roofHeight:6,
+		wallHeight: 8,
+		foundationDepth: 8,
+	},
+	to: {
+		length: 40,
+		width: 40,
+		endLength: 20,
+		endWidth: 20,
+		roofHeight:6,
+		wallHeight: 8,
+		foundationDepth: 8,
+	},
+	step: {
+		length: 2,
+		width: 2,
+		endLength: 2,
+		endWidth: 2,
+		wallHeight: .5,
+		roofHeight:1,
+		foundationDepth: 1,
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
